06b9f8250f268fa286f36aebc2ba6b0ce628a3b7
HighFire <lewis2.lb@gmail.com>
2019-09-20 16:49:20 -0700

    Update FakeChatRoom.java
    Finished base code, should print user message, and no-one else's (not networked in any fashion).

--- Experiments/FakeChatRoom.java
+++ Experiments/FakeChatRoom.java
@@ -10,10 +10,24 @@ public class ChatRoom
   public static void main( String args[] )
   {
   String Name;
+  String Message;
   int EnterRoom;
+  Scanner input = new Scanner( System.in );
   
   System.out.println( "Would you like to enter the room? (1 to enter, 0 to leave" )
   while ( EnterRoom == 1 )
+    {
+      System.out.println( "Please enter your username." );
+      input.nextLine();
+      Name = input.nextLine();
+      System.out.println( "Your username is " + Name + "." );
+      System.out.println( "Welcome to the room. Press 0 to leave")
+      while ( EnterRoom == 1 )
+      {
+        input.nextLine();
+        Message = input.nextLine();
+        System.out.println( Name + " : " + Message );
+      }
+    }
   }
-
 }
