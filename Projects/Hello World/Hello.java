0526baf1fd679264b62dd47e13f07ec8ff83e8a2
HighFire <lewis2.lb@gmail.com>
2019-09-23 11:19:27 -0700

    Add files via upload

---
+++ Projects/Hello World/Hello.java
@@ -0,0 +1,9 @@
+class Hello {
+    
+    // main: generate some simple output
+    
+     static void main(String[] args) {
+        System.out.print("Hello, world.");
+        System.out.println("What's up?");
+    }
+}
